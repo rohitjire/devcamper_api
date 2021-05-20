a8b89ef9f0a4793749121fc9dd77ad79bbf1f5ab
Rohit Jire <rohitjire55@gmail.com>
2021-05-20 12:18:20 +0530

    Selecting and sorting added

--- controller/bootcamp.js
+++ controller/bootcamp.js
@@ -9,15 +9,41 @@ const geocoder = require('../utils/geocoder')
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query
 
-  let querStr = JSON.stringify(req.query)
+  //Copy req.query
+  const reqQuery = { ...req.query }
+  // Fields to exclude
+  const removeFields = ['select', 'sort']
 
-  querStr = querStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
+  //Loop over removefields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param])
 
-  query = Bootcamp.find(JSON.parse(querStr))
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery)
 
+  // Create operators (gt, gte, etc)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`)
+
+  // Finding resouce 
+  query = Bootcamp.find(JSON.parse(queryStr))
+
+  // Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ')
+    query = query.select(fields)
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ')
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort('-createdAt')
+  }
+
+  // Executing query
   const bootcamps = await query
   res.status(200).json({
-    success: true, 
+    success: true,
     count: bootcamps.length,
     data: bootcamps,
   })
