03d254c2f8c7afb3a81e62e0147f7df56903387a
Rohit Jire <rohitjire55@gmail.com>
2021-05-27 21:36:53 +0530

    reset password stored

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const crypto = require('crypto')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
@@ -35,23 +36,43 @@ const UserSchema = new mongoose.Schema({
   },
 })
 
-  // Encrypt password using bcrypt
-UserSchema.pre('save', async function(next){
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next()
+  }
+
   const salt = await bcrypt.genSalt(10)
-  this.password =await bcrypt.hash(this.password, salt)
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
 // Sign JWT and return
-UserSchema.methods.getSignedJwtToken = function() {
-   return jwt.sign({ id : this._id }, process.env.JWT_SECRET,{
-     expiresIn: process.env.JWT_EXPIRE
-   })
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  })
 }
 
 // Match user entered password to hashed passowrd in database
-UserSchema.methods.matchPassword = async function(enteredPassword){
+UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+//Generate and hash passowrd token
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex')
+
+  // Hash token ans set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('[hex')
+
+  // Set the expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000
+
+  return resetToken
+}
 
 module.exports = mongoose.model('User', UserSchema)
--- seeder.js
+++ seeder.js
@@ -35,7 +35,7 @@ const importData = async () => {
   try {
     await Bootcamp.create(bootcamps)
     await Course.create(courses)
-    await Course.create(users)
+    await User.create(users)
     console.log('Data Imported...'.green.inverse)
     process.exit()
   } catch (error) {
